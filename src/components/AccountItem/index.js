58acfb71e5dcf6ed0929a2ba2286038fe02e701c
Thang Nguyen <nguyenducthangg30@gmail.com>
2024-08-13 21:46:05 +0700

    Xây dựng UI phần Header #3

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -10,7 +10,7 @@ function AccountItem() {
         <div className={cx('wrapper')}>
             <img
                 className={cx('avatar')}
-                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/60895fb000b5249b73f8ac5a3ea0d0c5~c5_300x300.webp?lk3s=a5d48078&nonce=99125&refresh_token=9e333d7cb0b91159ed9468c25a0e3479&x-expires=1723370400&x-signature=hWNXDvwjNQ9qP5a%2F93KTSs2g5yQ%3D&shp=a5d48078&shcp=c1333099"
+                src="https://cdn.phunuvagiadinh.vn/minhthu/auto/1_12_2022/37-2022-12-01-14-38.JPG"
                 alt="Hoaa" />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
